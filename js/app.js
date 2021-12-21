88c5d113764e29e655bbd3dd9266d52ac653e10b
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2021-12-21 06:15:09 +0200

    update page design

---
+++ js/app.js
@@ -0,0 +1,75 @@
+const projects =[]
+const buildpage = ()=>{
+
+  fetch("../res/json/portfolio.json")
+  .then((result)=>{
+    return result.json()
+  })
+  .then((res)=>{
+    projects.push(...res);
+
+  buildProjectsPortfolio();
+  })
+  
+  const buildProjectsPortfolio =()=>{
+    projects.forEach((project,index)=>{
+      if(!project.title) {
+        return
+      }
+      let id = project.id;
+      let img = project.image;
+      let title = project.title;
+      let description = project.description;
+      let url = project.url;
+      console.log(title)
+      portfolioRow.append(createPortfolio(index,img,title,description,url))
+    })
+    
+     
+    
+  }
+
+
+let portfolioRow = $("#portfolio-row")
+console.log(portfolioRow)
+
+const createPortfolio = (index,img,title,dscrption,url="#")=>{
+let template=`
+<!-- project ${index} -->
+
+<div class="col">
+<div class="card shadow">
+  <img
+    class="card-img-top"
+    src=${img}
+    alt=""
+  />
+  <div class="card-body bg-primary">
+    <p class="card-title text-light">${title}</p>
+    <p class="card-text">${dscrption}</p>
+    <div
+      class="d-flex justify-content-between align-items-center"
+    >
+      <a
+        type="button"
+        class="btn btn-md btn-outline-warning"
+        href=${url}
+        target="_blank"
+      >
+        View
+      </a>
+    </div>
+  </div>
+</div>
+</div>
+            <!-- /project ${index} -->
+`
+return template;
+}
+
+
+}
+
+$(document).ready(()=>{
+  buildpage();
+})
